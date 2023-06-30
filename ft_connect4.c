e2796cc2d4a84dc48d3d5578d0d025429b99c5c2
Hurumy <sarrendish@gmail.com>
2023-06-30 21:26:27 +0900

    basic files

---
+++ ft_connect4.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_connect4.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/30 20:10:52 by komatsud          #+#    #+#             */
+/*   Updated: 2023/06/30 20:17:29 by komatsud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_connect4.h"
+
+int	main(int argc, char **argv)
+{
+//init, get infos, decide which one to play first
+//while
+	//draw
+	//get_move
+	//get_move - opponents
+	//
+//is_game_ended
+//free,exit
+	return (0);
+}
